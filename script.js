496c4f01736a721631979e23141b6cb9455cc0dd
Faisal Khan <faisalkhan1222@gmail.com>
2016-06-06 14:48:42 -0400

    contact page done

---
+++ script.js
@@ -0,0 +1,12 @@
+function mail(){
+
+var subject = document.getElementById("subject").value;
+var comments = document.getElementById("comments").value;
+
+console.log('subject: ' + subject);
+console.log('comments: ' + comments);
+
+window.open('mailto:test@example.com?subject='+subject+'&body='+comments);
+
+return false;
+}
